5269b4fa463939b8d7b5d1d2e68e95843c4969bd
SpiZeak <max.t96@hotmail.com>
2017-12-29 12:55:03 +0100

    collectibg post on main page

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -22,6 +22,31 @@
 				'after'  => '</div>',
 			) );
 		?>
+			<?php
+			$args = array( 'posts_per_page' => 5 );
+
+			$myposts = get_posts( $args );
+			$i = 0;
+			foreach ( $myposts as $post ) : setup_postdata( $post );
+
+				if( $i === 0 ) : ?>
+					<div class="main-post">
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+						<p><?php the_excerpt(); ?></p>
+					</div>
+				<?php
+				$i = 1;
+				else: ?>
+					<div class="post">
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+						<p><?php the_excerpt(); ?></p>
+					</div>
+				<?php
+				endif; ?>
+
+			<?php endforeach; 
+			wp_reset_postdata();
+			?>
 	</div><!-- .entry-content -->
 
 	<?php if ( get_edit_post_link() ) : ?>
