2b552629efe5ff22a3250b538c8b900e589d8d27
SpiZeak <max.t96@hotmail.com>
2018-01-29 14:50:59 +0100

    Navbar is now sticky, slider reworked.

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,8 +1,58 @@
 (function( $ ) {
 
-    // var slider = $('.stf-slider'),
-    // sliderWidth = slider.width(),
-    // images = $('.stf-slider > img'),
-    // count = $('.stf-slider > img').length;
+    var slider = $('.stf-slider'),
+    list = $('.stf-slider ul'),
+    images = $('.stf-slider ul img'),
+    prevBtn = $('.stf-slider .previous'),
+    nextBtn = $('.stf-slider .next'),
+    left = 0;
+
+    // Remove arrows if no more than one image
+    if(images.length === 1) {
+        prevBtn.css({'display': 'none'});
+        nextBtn.css({'display': 'none'});
+    }
+
+    // Adjust width of container after amount of images
+    list.css({
+        'width': 100 * images.length + '%'
+    });
+
+    // Adjust image size after amount of images
+    images.css({
+        'width': 100 / images.length + '%'
+    });
+
+    prevBtn.click(function() {
+        if(left < 0) {
+            left += 100;
+
+            $('.stf-slider ul').css({
+                'left': left + '%'
+            });
+        } else {
+            left = -(images.length * 100) + 100;
+
+            $('.stf-slider ul').css({
+                'left': left + '%'
+            });
+        }
+    });
+
+    nextBtn.click(function() {
+        if(left > -((images.length * 100) - 100)) {
+            left -= 100;
+
+            $('.stf-slider ul').css({
+                'left': left + '%'
+            });
+        } else {
+            left = 0;
+
+            $('.stf-slider ul').css({
+                'left': left
+            });
+        }
+    });
 
 } )( jQuery );
--- template-parts/content-front.php
+++ template-parts/content-front.php
@@ -13,8 +13,6 @@
 
 	<div class="entry-content">
 		<?php
-			the_content();
-
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sverigestamfagelforening' ),
 				'after'  => '</div>',
@@ -23,9 +21,25 @@
 			<div class="top-info">
 				<div class="stf-slider">
 					<ul>
-						<img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/parrot.png'?>" alt="Parrot image">
-						<img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/parrot.png'?>" alt="Parrot image">
+
+						<?php
+						if( have_rows('slider') ):
+							while ( have_rows('slider') ) : the_row(); ?>
+
+								<img src="<?php the_sub_field('bild'); ?>" alt="Parrot image">
+
+							<?php
+							endwhile;
+						else : ?>
+
+							<img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/parrot.png'?>" alt="Parrot image">
+
+						<?php
+						endif; ?>
+
 					</ul>
+					<button class="previous"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
+					<button class="next"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
 				</div>
 				<div class="container">
 					<div>
@@ -39,8 +53,8 @@
 					<div>
 						<h2>Facebook</h2>
 						<p>
-						Har du en fågel? Eller kanske vill du skaffa en fågel?
-						Glöm inte att gå med i vår facebookgrupp för att få tips och råd bland andra med samma intresse!
+							Har du en fågel? Eller kanske vill du skaffa en fågel?
+							Glöm inte att gå med i vår facebookgrupp för att få tips och råd bland andra med samma intresse!
 						</p>
 					</div>
 				</div>
@@ -52,7 +66,7 @@
 					<a href="#">
 						<img class="main-post-img" src="<?php echo get_stylesheet_directory_uri() . '/dist/img/image.png' ?>" alt="">
 						<div class="main-post-text">
-							<span class="publiced-date">Publicerad November 19, 2017   </span>
+							<span class="publiced-date">November 19, 2017   </span>
 							<span class="category">FÅGELTRÄFFAR </span>	
 								<h4>Nya styrelsen</h4>
 								<p>
@@ -62,12 +76,27 @@
 						</div>
 					</a>
 				</div>
-
 				<div class="basic-post">
 					<a href="#">
 							<img class="post-img" src="<?php echo get_stylesheet_directory_uri() . '/dist/img/image.png' ?>" alt="">
 							<div class="post-text">
-								<span class="publiced-date">Publicerad November 19, 2017   </span>
+								<span class="publiced-date">November 19, 2017   </span>
+								<span class="category">FÅGELTRÄFFAR </span>	
+									<div class="post-content">
+									<h4>Nya styrelsen</h4>
+									<p>
+										Lo-fi cray kinfolk readymade pug quinoa actually small batch narwhal celiac slow-carb vexillologist.
+										Hell of etsy hashtag kale chips sriracha occupy jianbing vape...
+									</p>	
+									</div>						
+							</div>
+					</a>
+				</div>
+				<div class="basic-post">
+					<a href="#">
+							<img class="post-img" src="<?php echo get_stylesheet_directory_uri() . '/dist/img/image.png' ?>" alt="">
+							<div class="post-text">
+								<span class="publiced-date">November 19, 2017   </span>
 								<span class="category">FÅGELTRÄFFAR </span>	
 									<div class="post-content">
 									<h4>Nya styrelsen</h4>
@@ -81,9 +110,9 @@
 				</div>
 				<button class="more-post">Visa fler</button>
 
-			</div>
+			</div><!-- .main-post -->
 
-				<?php
+			<?php
 			$args = array(
 				'posts_per_page' => 5
 			);
