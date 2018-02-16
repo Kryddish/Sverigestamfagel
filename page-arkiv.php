232abe1e50c00a7d9754f26acd9e029d20f892fe
Memlisen <emmelie.sundell@hotmail.com>
2018-02-16 19:31:27 +0100

    layout archive page started

--- page-arkiv.php
+++ page-arkiv.php
@@ -16,6 +16,8 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<?php dynamic_sidebar( 'custom-video' ); ?>
+
 
 			<?php
 			while ( have_posts() ) : the_post();
@@ -25,6 +27,62 @@ get_header(); ?>
 			endwhile; // End of the loop.
 			?>
 
+			<div class="posts-container">
+
+				<?php
+				$posts = get_posts( array(
+					'post_type' 		=> 	array( 'meets', 'post' ),
+					'posts_per_page'	=> 	3,
+					'meta_key' 			=> 	'date',
+					'orderby' 			=> 	'meta_value',
+					'order'				=>	'DESC'
+				) );
+
+				foreach( $posts as $post ) : setup_postdata( $post );
+
+					get_template_part( 'template-parts/content' );
+
+					if ( get_edit_post_link() ) : ?>
+
+						<footer class="entry-footer">
+							<span>Senast ändrad <?php the_modified_date(); ?></span>
+
+							<?php
+							edit_post_link(
+								sprintf(
+									/* translators: %s: Name of current post */
+									esc_html__( 'Redigera inlägg %s', 'sverigestamfagelforening' ),
+									the_title( '<span class="screen-reader-text">"', '"</span>', false )
+								),
+								'<span class="edit-link">',
+								'</span>'
+							); ?>
+
+						</footer><!-- .entry-footer -->
+
+					<?php
+					endif;
+					
+				endforeach; 
+				wp_reset_postdata(); ?>
+					
+			</div>
+
+			<div class="archive-post">
+				<h3>Detta är ett inlägg</h3>
+				<em><span>Publicerad: 20/1 - 2018</span></em>
+				<hr>
+				<img src="https://assets.bakker.com/ProductPics/810x978/86762-02-BAKI_20150112160335.jpg">
+				<p>Detta är postens text. Dock ska den bara visa de 100 första orden och inte mer för annars blir designen
+					ful. Förstår du? Dock är detta inte 100 ord men detta är bara ett test. Hejdå
+				</p>
+			</div>
+			
+			<div class="side-archive">				
+				<?php dynamic_sidebar( 'sidebar-2' ); ?>
+			</div>
+			
+			
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
