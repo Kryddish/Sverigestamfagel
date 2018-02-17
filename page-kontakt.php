958cadc4c855aa87e017f530f3b197cfda9ac844
Memlisen <emmelie.sundell@hotmail.com>
2018-02-17 14:12:19 +0100

    contact-form styling

--- page-kontakt.php
+++ page-kontakt.php
@@ -16,18 +16,15 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'none' );
-
-			endwhile; // End of the loop.
-			?>
-
+		<section id="contact">
+	
+			<div class="contact-container">
+				
+			</div>
+		</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
