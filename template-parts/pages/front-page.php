e1455f1200bb550e4e91647c7079e3945457d860
Emmelie Sundell <emmelie.sundell@hotmail.com>
2018-03-20 16:30:12 +0100

    edit how to display new

--- template-parts/pages/front-page.php
+++ template-parts/pages/front-page.php
@@ -158,9 +158,14 @@
 				foreach( $posts as $post ) : setup_postdata( $post );
 
 					if( $post->post_type !== 'meets' ) :
-						if( $index < $news_count ) :
-							get_template_part( 'template-parts/content/content' );
-							$index++;
+						if( $index < $news_count ) : ?>
+							<a href="<?php the_permalink(); ?>"><h5><?php the_title(); ?></h5></a>
+							<?php the_excerpt();
+								the_category();
+								the_date(); ?> 
+								<hr>
+								<?php
+							 $index++;
 						else:
 							break;
 						endif;
