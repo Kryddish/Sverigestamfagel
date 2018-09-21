97d1d6cbabe3a90c9a80e40972bd6b89ec7271f4
Memlisen <emmelie.sundell@hotmail.com>
2018-09-21 16:14:19 +0200

    fixed membership page

--- wp-content/themes/sverigestamfagel/page-medlemskapet.php
+++ wp-content/themes/sverigestamfagel/page-medlemskapet.php
@@ -11,7 +11,7 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content/content', 'none' );
+				get_template_part( 'template-parts/pages/om-oss/medlemskapet', 'none' );
 
 			endwhile; // End of the loop.
 			?>
