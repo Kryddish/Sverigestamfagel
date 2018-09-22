664ad081de3be9a05998573346ca299a8e0bbc14
Memlisen <emmelie.sundell@hotmail.com>
2018-09-22 16:32:45 +0200

    contact page

--- wp-content/themes/sverigestamfagel/template-parts/pages/kontakt.php
+++ wp-content/themes/sverigestamfagel/template-parts/pages/kontakt.php
@@ -1,2 +1,5 @@
+<div class="contact-form">
+    <?php echo do_shortcode( '[contact-form-7 title="Kontakt"]' ); ?>
 
-<?php echo do_shortcode( '[contact-form-7 title="Kontakt"]' ); ?>
+    <!-- <img src="https://www.partille-tool.se/media/wysiwyg/illustrationer/SverigeKarta.png" alt=""> -->
+</div>
