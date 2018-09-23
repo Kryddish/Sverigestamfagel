e7ebbebd1c7ad30ade698cf0d0bc87a525a0232d
Memlisen <emmelie.sundell@hotmail.com>
2018-09-23 18:35:58 +0200

    membership page

---
+++ wp-content/themes/sverigestamfagel/template-parts/pages/om-oss/medlemskapet.php
@@ -0,0 +1,45 @@
+<div class="membership">
+    <div class="header">
+        <h3>Bli medlem i Sveriges Tamfågelförening (STF)</h3>
+            
+        <div class="intro">
+        <img src="https://image.ibb.co/cjPoU9/37945556_1730215640424254_5003650476231622656_n.jpg" alt="">
+            <h5>
+                Som medlem hos oss blir du även medlem i Riksförbundet Svensk fågelhobby. Vilka förmåner du som medlem får kan du läsa mer om 
+                <a href="">här</a>. <br>
+                Du blir även kontaktad av STF via nyhetsbrevet som skickas ut via e-post med jämna mellanrum, där det står information om våra aktuella träffar mm.
+                <button>Bli medlem här</button>    
+            </h5>
+        </div>
+    </div>
+
+
+    <div class="dues">
+        <h2>Medlemsavgiften</h2>
+            <p>Medlemsavgiften är 300 kronor per kalenderår. Medlemsavgiften är uppdelad i följande poster:</p>
+            <p>50 kronor till Riksförbundet Svensk Fågelhobby (SF).</p>
+            <p>210 kronor för tidningen Fågelhobby. Mer om tidningen kan du läsa nedan.</p>
+            <p>40 kronor går lokalt till STF. Dessa pengar används bland annat till att hålla hemsidan uppe. Styrelsen arbetar ideellt.</p>
+            <p>När du blir medlem i föreningen efter den 30/6, så har du möjlighet att betala bara halva avgiften för tidningen (105 kronor istället för 210 kronor). Den totala medlemsavgiften blir då  195 kronor.</p>
+            <p>När du gör så kommer du enbart att få Medlemstidningen från och med att du har blivit medlem hos oss. Betalar du fullt medlemsavgiften på 300 kronor efter den 30/6, så skickas även årets tidigare utgåvor av fågelhobby tidningen hem till dig</p>
+            <p>Är du redan medlem i en annan förening som är ansluten till Riksförbundet Svensk Fågelhobby betalar du 60 kronor i medlemsavgift. <a href="http://fagelhobby.nu/forbundet/foreningar/">Lista på anslutna föreningar</a></p>
+            <p>Om fler personer inom samma familj, bosatta på samma adress blir medlemmar betalar en full medlemsavgift, övriga betalar reducerad föreningsavgift (familjemedlemskap), 70 kronor i medlemsavgift.</p>
+            <p>Medlemsavgiften för kommande år ska vara inbetald på föreningens plusgirokonto senast den 10:e januari. (Till exempel medlemsavgift för år 2018 ska vara inbetald senast 2018-01-10)</p>
+    </div>
+
+    <div class="howmember">
+        <h2>Hur blir man medlem?</h2>
+           <h6> 1. Läs gärna igenom våra stadgar som du hittar <a href="https://sverigestamfagel.se/om-foreningen/stadgar/">här<img src="https://cdn2.iconfinder.com/data/icons/adobe-acrobat-pdf/154/adobe-acrobat-pdf-file-document-512.png" alt=""></a></h6>
+        <p>
+            Dreamcatcher kale chips biodiesel enamel pin austin, scenester mumblecore keytar DIY beard intelligentsia humblebrag air plant cold-pressed. 
+            Fam shabby chic hella helvetica tousled jianbing 3 wolf moon, cliche listicle mlkshk williamsburg thundercats sartorial bicycle rights roof party. 
+        </p>
+        <p>
+            Wayfarers flannel squid kickstarter vinyl, schlitz semiotics chartreuse photo booth echo park mustache portland austin gochujang. Schlitz knausgaard ennui cred subway tile direct trade. Taxidermy +1 tofu, copper mug plaid flexitarian ramps thundercats celiac slow-carb lyft meggings raw denim. Edison bulb raclette portland venmo crucifix actually vexillologist, photo booth sriracha twee. Single-origin coffee leggings shabby chic direct trade wolf.
+            Oh. You need a little dummy text for your mockup? How quaint.
+        </p>
+
+        <button>Bli medlem nu</button>
+    </div>
+
+</div>
