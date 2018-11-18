0116306a4d4dc31c12df37b3d8cb80de0f84c918
Memlisen <emmelie.sundell@hotmail.com>
2018-11-18 13:36:43 +0100

    beging of merge

---
+++ wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('/customers/1/4/b/sverigestamfagel.se/httpd.www/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/customers/1/4/b/sverigestamfagel.se/httpd.www/wp-content/wflogs/');
+	include_once '/customers/1/4/b/sverigestamfagel.se/httpd.www/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
+?>
