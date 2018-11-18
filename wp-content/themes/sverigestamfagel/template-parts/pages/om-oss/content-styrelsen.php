23a2743217599777ce91efcc57dbebef3280a7f2
Memlisen <emmelie.sundell@hotmail.com>
2018-11-18 17:13:46 +0100

    Styrelsen only acf

--- wp-content/themes/sverigestamfagel/template-parts/pages/om-oss/content-styrelsen.php
+++ wp-content/themes/sverigestamfagel/template-parts/pages/om-oss/content-styrelsen.php
@@ -1,84 +1,46 @@
-<h2>STFs Styrelse. </h2>
-<p>
-    Var vänlig och kontakta oss under våra mailadresser nedan om du vill komma i kontakt med en av oss i styrelsen. Alternativt maila till 
-    vår infobrevlåda under info@sverigestamfagel.se eller använda kontaktformuläret på den här hemsidan.
-</p>
+<div class="header">
+    <h2>STF:s Styrelse. </h2>
+    <p>
+        Vill du komma i kontakt med någon i styrelsen? Här når du oss enskilt,
+        annars maila till <a href="mailto:<?php echo get_bloginfo( 'admin_email' ); ?>"><?php echo get_bloginfo( 'admin_email' ); ?></a>
+        eller använd <a href="<?php echo site_url(); ?>/kontakt">kontaktformuläret</a>.
+    </p>
+</div>
 
 <div class="board-members">
+    
+    <?php 
+    if( have_rows('members') ): 
+        while ( have_rows('members') ) : the_row(); ?>
 
-    <div class="member">
-        <img src="https://www.sverigestamfagel.se/wp-content/uploads/2015/11/STF-logo.gif" alt="">
-        <div class="member-info">
-            <p><strong>Ordförande:</strong> Andréas Forsman<br>
-            <strong>E-post: </strong> andreas@sverigestamfagel.se<br>
-            *insert info here*</p>
-        </div>
-    </div>	
-
-    <div class="member">
-        <img src="https://www.sverigestamfagel.se/wp-content/uploads/2015/11/STF-logo.gif" alt="">
+            <div class="member">
+                <img src="<?php
+                if( get_sub_field( 'image' ) ) : 
+                    the_sub_field( 'image' );
+                else : echo 'https://www.sverigestamfagel.se/wp-content/uploads/2015/11/STF-logo.gif';
+                endif; ?>" alt="">
+                
+                <div class="member-info">
+                    <p>
+                        <?php
+                        if( get_sub_field( 'role' ) ) : ?>
+                            <strong><?php the_sub_field( 'role' ); ?></strong><br>
+                        <?php 
+                        endif;
+                        if( get_sub_field( 'name' ) ) : ?>
+                            <?php the_sub_field( 'name' ); ?><br>
+                        <?php
+                        endif;
+                        if( get_sub_field( 'email' ) ) : ?>
+                            <a href="mailto:<?php the_sub_field( 'email' ); ?>"><?php the_sub_field( 'email' ); ?></a>
+                        <?php
+                        endif; ?>
+                    </p>
+                </div>
+            </div>
         
-        <div class="member-info">
-            <p><strong>Sekreterare:</strong> Ulrika Sundell<br>
-            <strong>E-post:</strong> ulrika@sverigestamfagel.se<br>
-            *insert info here*</p>
-        </div>
-    </div>
-
-    <div class="member">
-        <img src="https://www.sverigestamfagel.se/wp-content/uploads/2015/11/STF-logo.gif" alt="">
-        <div class="member-info">
-            <p><strong>Kassör:</strong> Steffen Langhammer<br>
-            <strong>E-post:</strong> steffen@sverigestamfagel.se<br>
-            *insert info here*</p>
-        </div>
-    </div>
-
-    <div class="member">
-        <img src="https://www.sverigestamfagel.se/wp-content/uploads/2015/11/STF-logo.gif" alt="">
-        <div class="member-info">
-            <p><strong>Ledamot:</strong> Krystian Brzezinski<br>
-            <strong>E-post: </strong> krystian@sverigestamfagel.se<br>
-            *insert info here*</p>
-        </div>
-        </div>
-
-    <div class="member">
-        <img src="https://www.sverigestamfagel.se/wp-content/uploads/2015/11/STF-logo.gif" alt="">
-        <div class="member-info">
-            <p><strong>Ledamot:</strong> Emmelie Sundell<br>
-            <strong>E-post: </strong> emmelie@sverigestamfagel.se<br>
-            *insert info here*</p>
-        </div>
-    </div>
-        
-    <div class="member">
-        <img src="https://www.sverigestamfagel.se/wp-content/uploads/2015/11/STF-logo.gif" alt="">
-        
-        <div class="member-info">
-            <p><strong>Ledamot:</strong> Emmelie Sundell<br>
-            <strong>E-post:</strong> emmelie@sverigestamfagel.se<br>
-            *insert info here*</p>
-        </div>
-    </div>
-
-    <div class="member">
-        <img src="https://www.sverigestamfagel.se/wp-content/uploads/2015/11/STF-logo.gif" alt="">
-        <div class="member-info">
-            <p><strong>Ledamot:</strong> Madelene Erlandsson<br>
-            <strong>E-post:</strong> madelene@sverigestamfagel.se<br>
-            *insert info here*</p>
-        </div>
-    </div>
-
-    <div class="member">
-        <p><strong>Revisor:</strong>Karl-Erik Tenglin<br>
-        <strong>Revisorssuppleant:</strong> Agneta Jernberg</p>
-    </div>
-
-    <div class="member">
-        <p><strong>Valberedning:</strong> Amanda Ell<br>
-        <strong>E-post:</strong> amanda@sverigestamfagel.se</p>
-    </div>
+        <?php
+        endwhile;
+    endif; ?>
     
 </div><!-- .board-members -->
