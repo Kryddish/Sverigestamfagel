7a20579bccc433aeec7831df51469e4363fbaabd
Memlisen <emmelie.sundell@hotmail.com>
2018-11-18 18:54:19 +0100

    added map to cantact

--- wp-content/themes/sverigestamfagel/template-parts/pages/kontakt.php
+++ wp-content/themes/sverigestamfagel/template-parts/pages/kontakt.php
@@ -1,5 +1,10 @@
 <div class="contact-form">
-    <?php echo do_shortcode( '[contact-form-7 title="Kontakt"]' ); ?>
+    
+    <h2>Kontakta oss:</h2>
+    <p>Har du några funderingar? Skriv tills oss nedan så svarar vi så snabbt vi kan.  </p>
 
-    <!-- <img src="https://www.partille-tool.se/media/wysiwyg/illustrationer/SverigeKarta.png" alt=""> -->
+    <div class="form">
+        <?php echo do_shortcode( '[contact-form-7 title="Kontakt"]' ); ?>
+    </div>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Flag-Map_of_Sweden.svg/303px-Flag-Map_of_Sweden.svg.png" alt=""> 
 </div>
