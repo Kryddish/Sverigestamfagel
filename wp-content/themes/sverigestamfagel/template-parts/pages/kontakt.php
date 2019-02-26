c2d70378ad1fb1186febdd78ac38e687586b236d
Max Trewhitt <max.trewhitt@osolo.se>
2019-02-26 20:36:44 +0100

    Add: Contact page styling

--- wp-content/themes/sverigestamfagel/template-parts/pages/kontakt.php
+++ wp-content/themes/sverigestamfagel/template-parts/pages/kontakt.php
@@ -1,10 +1,11 @@
 <div class="contact-form">
+	<div>
+		<h2>Kontakta oss:</h2>
+		<p>Har du några funderingar? Skriv tills oss nedan så svarar vi så snabbt vi kan.</p>
 
-    <h2>Kontakta oss:</h2>
-    <p>Har du några funderingar? Skriv tills oss nedan så svarar vi så snabbt vi kan.  </p>
-
-    <div class="form">
-        <?= do_shortcode( '[contact-form-7 title="Kontakt"]' ); ?>
-    </div>
+		<div class="form">
+			<?= do_shortcode( '[contact-form-7 title="Kontakt"]' ); ?>
+		</div>
+	</div>
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Flag-Map_of_Sweden.svg/303px-Flag-Map_of_Sweden.svg.png" alt="">
 </div>
