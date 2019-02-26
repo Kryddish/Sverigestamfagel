54fdd2623ca3c93ecced678cd2bedc7d55cfcecb
Max Trewhitt <max.trewhitt@osolo.se>
2019-02-26 20:08:36 +0100

    Add: Article page template

--- wp-content/themes/sverigestamfagel/inc/filters.php
+++ wp-content/themes/sverigestamfagel/inc/filters.php
@@ -1,3 +1,8 @@
 <?php
 // Add shortcode functionality to ACF fields
 add_filter('acf/format_value/type=textarea', 'do_shortcode', 10, 3);
+
+// Change default page template label
+add_filter('default_page_template_title', function() {
+    return __('Artikelsida', 'stf');
+});
--- wp-content/themes/sverigestamfagel/template-parts/content/content-page.php
+++ wp-content/themes/sverigestamfagel/template-parts/content/content-page.php
@@ -1,51 +1,49 @@
 <?php
-/**
- * Template part for displaying page content in page.php.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package stf
- */
+global $post;
+$parents = get_post_ancestors(get_the_ID());
 
-?>
+if ($parents == null) {
+	$first_parent_id = $post->ID;
+} else {
+	$first_parent_id = end($parents);
+}
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-	</header><!-- .entry-header -->
+$pages = get_pages([
+	'child_of' => $first_parent_id
+]);
 
-	<div class="entry-content">
+$first_page = get_post($first_parent_id); ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('c-article'); ?>>
+	<?php
+	if ($pages): ?>
+		<aside class="c-article__sidebar">
+			<h5>
+				<a href="<?= get_permalink($first_parent_id) ?>">
+					<?= $first_page->post_title ?>
+				</a>
+			</h5>
+			<ul>
+				<?php
+				foreach ($pages as $page):
+					if ($page->ID == $post->ID): ?>
+						<li class="current">
+							<a href="<?= get_permalink($page->ID) ?>"><?= get_the_title($page->ID) ?></a>
+						</li>
+					<?php
+					else:  ?>
+						<li>
+							<a href="<?= get_permalink($page->ID) ?>"><?= get_the_title($page->ID) ?></a>
+						</li>
+						<?php
+					endif;
+				endforeach; ?>
+			</ul>
+		</aside>
 		<?php
-			the_content();
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'stf' ),
-				'after'  => '</div>',
-			) );
-		?>
-			<?php
-			$args = array( 'posts_per_page' => 5 );
-
-			$myposts = get_posts( $args );
-			$i = 0;
-			foreach ( $myposts as $post ) : setup_postdata( $post );
-
-				if( $i === 0 ) : ?>
-					<div class="main-post">
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						<p><?php the_excerpt(); ?></p>
-					</div>
-				<?php
-				$i = 1;
-				else: ?>
-					<div class="post">
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						<p><?php the_excerpt(); ?></p>
-					</div>
-				<?php
-				endif; ?>
-
-			<?php endforeach; 
-			wp_reset_postdata();
-			?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+	endif; ?>
+	<div class="c-article__content">
+		<h1><?= get_the_title() ?></h1>
+		<?= the_content() ?>
+	</div>
+</article>
