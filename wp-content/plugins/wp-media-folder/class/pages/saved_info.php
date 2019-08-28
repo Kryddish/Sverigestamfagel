e21b7e0104ca1b67df2bf6e7c2b28c3001a85dfd
Max Trewhitt <max.trewhitt@osolo.se>
2019-08-28 22:44:22 +0200

    Remove: WP Media Folder plugin

--- wp-content/plugins/wp-media-folder/class/pages/saved_info.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Prohibit direct script loading */
-defined('ABSPATH') || die('No direct script access allowed!');
-?>
-<div class='success updated'>
-    <p><?php esc_html_e('Saved successfully', 'wpmf'); ?></p>
-</div>
