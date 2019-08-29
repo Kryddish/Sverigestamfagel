87ddf59bdd181e44efe432e7a43de5654def1f75
Max Trewhitt <max.trewhitt@osolo.se>
2019-08-29 19:57:02 +0200

    Add: Breadcrumbs

--- wp-content/themes/sverigestamfagel/inc/filters.php
+++ wp-content/themes/sverigestamfagel/inc/filters.php
@@ -6,3 +6,21 @@ add_filter('acf/format_value/type=textarea', 'do_shortcode', 10, 3);
 add_filter('default_page_template_title', function() {
     return __('Artikelsida', 'stf');
 });
+
+// add_filter( 'wp_nav_menu_objects', function($items) {
+//     $parents = array();
+
+//     foreach ( $items as $item ) {
+//         if ( $item->menu_item_parent && $item->menu_item_parent > 0 ) {
+//             $parents[] = $item->menu_item_parent;
+//         }
+//     }
+
+//     foreach ( $items as $item ) {
+//         if ( in_array( $item->ID, $parents ) ) {
+//             $item->title .= ' <i class="icon-angle-down" />';
+//         }
+//     }
+
+//     return $items;
+// });
--- wp-content/themes/sverigestamfagel/template-parts/article.php
+++ wp-content/themes/sverigestamfagel/template-parts/article.php
@@ -5,6 +5,10 @@
 		<?php
 		while ( have_posts() ) : the_post();
 
+			if ( function_exists('yoast_breadcrumb') ) {
+				yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+			}
+
 			get_template_part('template-parts/content/content-page');
 
 		endwhile; ?>
