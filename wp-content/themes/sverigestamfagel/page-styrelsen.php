c3e0e367c0f43ee841bc89e7ae8eb4a451b953f2
Max Trewhitt <max.trewhitt@osolo.se>
2019-08-29 20:20:48 +0200

    Fix: duplicate files

--- wp-content/themes/sverigestamfagel/page-styrelsen.php
+++ wp-content/themes/sverigestamfagel/page-styrelsen.php
@@ -11,6 +11,10 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
+				if ( function_exists('yoast_breadcrumb') ) {
+					yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+				}
+
 				get_template_part( 'template-parts/pages/om-oss/styrelsen' );
 
 			endwhile; // End of the loop.
